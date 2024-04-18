49c8117ad328fd52bde97ba76c4b929ed79760ba
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-18 23:35:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-if (num <= 1) return num; // Base case: return num for 0 and 1
-
+function fibonacci(num) {
+	if (num <= 1) return num; // Base case: return num for 0 and 1
 return fibonacci(num - 1) + fibonacci(num - 2); // Recursive case: return sum of previous two Fibonacci numbers
 }
 
